9f21f10166c1835d6a103841a59e8967a169d539
jimini <gjm0313@naver.com>
2024-01-27 22:05:05 +0900

    Prisma_외래키 & 인증, 인가

---
+++ 새 폴더/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import UsersRouter from "./src/routes/user.router.js";
+
+const app = express();
+const PORT = 3018;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", [UsersRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT + "포트로 서버가 시작되었습니다.");
+});
